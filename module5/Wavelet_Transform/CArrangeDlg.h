b380aa257d67b4fc02eefbef1017eb9e8aaf962e
Seo-a-ro <82528589+Seo-a-ro@users.noreply.github.com>
2021-05-18 20:33:49 +0900

    Add files via upload

---
+++ module5/Wavelet_Transform/CArrangeDlg.h
@@ -0,0 +1,29 @@
+﻿#pragma once
+
+
+// CArrangeDlg 대화 상자
+
+class CArrangeDlg : public CDialog
+{
+	DECLARE_DYNAMIC(CArrangeDlg)
+
+public:
+	CArrangeDlg(CWnd* pParent = nullptr);   // 표준 생성자입니다.
+	virtual ~CArrangeDlg();
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_DIALOG2 };
+#endif
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 지원입니다.
+
+	DECLARE_MESSAGE_MAP()
+public:
+	virtual BOOL OnInitDialog();
+	afx_msg void OnPaint();
+	unsigned char** m_tempImage;
+	int Height;
+	int Width;
+};
